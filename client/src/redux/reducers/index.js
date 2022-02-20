926ea3c9afdc1f2a547eaf020efce2d926891026
VictorySuccess000 <bestlucky1030@gmail.com>
2022-02-21 01:16:40 +0900

    2/20

--- client/src/redux/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from "redux";
-
-import util from "./util";
-import auth from './auth'
-
-export default combineReducers({
-    util,
-    auth
-})
--- client/src/redux/types.js
+++
@@ -1,5 +0,0 @@
-export const USER_LOADED = "USER_LOADED";
-export const SET_VCOIN = "SET_VCOIN";
-export const USER_AUTHENTICATED = 'USER_AUTHENTICATED'
-export const SET_PROFILE = "SET_PROFILE";
-export const SET_Isexpand = "SET_Isexpand";
