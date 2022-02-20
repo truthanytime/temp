c99cb46148c08d91024b88b0b03c79c2209992dd
VictorySuccess000 <bestlucky1030@gmail.com>
2022-02-21 01:49:42 +0900

    only

--- routes/api/geocode.js
+++
@@ -1,9 +0,0 @@
-const NodeGeocoder = require('node-geocoder');
-
-const options = {
-  provider: 'google',
-  apiKey: 'AIzaSyCAa8ZgPpTBMcCV7lrNJXvE70JoRs9Wl8I', // for Mapquest, OpenCage, Google Premier
-  formatter: null // 'gpx', 'string', ...
-};
-
-const geocoder = NodeGeocoder(options);
